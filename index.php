45683a6be271f10c8211f3d113944e7554e90c98
chul <chul@ibugroup.com>
2022-08-03 03:16:55 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Stabig - Responsive Landing page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Premium Bootstrap 5 Template" />
+    <meta name="keywords" content="bootstrap 5, premium, marketing, multipurpose" />
+    <meta content="Themesdesign" name="author" />
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="images/favicon.ico" />
+
+    <!-- tinyslider -->
+    <link rel="stylesheet" href="css/tiny-slider.css" />
+
+    <!-- css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.min.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="67">
+    <style>
+        .hero-1 {
+            /* height: 150vh; */
+            height: auto;
+            width: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        .input-group-text {
+            height: 45px;
+        }
+
+        .hide {
+        display: none;
+    }
+    </style>
+    <!-- start navbar -->
+    <nav class="navbar navbar-expand-lg fixed-top sticky" id="navbar">
+        <div class="container">
+            <a href="layout-1.html" class="navbar-brand me-5">
+                <img src="images/logo/logo1.png" class="logo-dark" alt="" height="42" />
+            </a>
+            <a href="javascript:void(0)" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggle-icon"><i data-feather="menu"></i></span>
+            </a>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav navbar-center me-auto mt-lg-0 mt-2">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" href="#feature">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#service">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#faq">Faq</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#pricing">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#client">Client</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Join Us</a>
+                    </li> -->
+                </ul>
+                <div class="mb-4 mb-lg-0">
+                    <a href="#" class="btn btn-sm nav-btn btn-primary mb-4 mb-lg-0 ms-auto" data-bs-toggle="modal"
+                        data-bs-target="#exampleModal">Contact Us</a>
+                </div>
+            </div>
+        </div>
+        <!-- end container -->
+    </nav>
+    <!-- end navbar -->
+
+    <?php include 'home.php';?>
+
+ 
+
+
+    <?php include 'details_power.php';?>
+    <?php include 'details_uses.php';?>
+
+    <?php include 'output_power.php';?>
+    <?php include 'output_uses.php';?>
+ 
+
+   
+
+  
+
+    <!-- start subscription -->
+    <!-- <section class="service-section overflow-hidden" id="contact">
+        <div class="container">
+            <div class="row align-items-center bg-light rounded p-4">
+                <div class="col-lg-6">
+                    <h5 class="text-primary mb-3">Recent</h5>
+                    <h3 class="mb-4">5000<sup>+</sup> Project <br> all over the world</h3>
+                    <h6 class="text-muted fw-normal">Join the community now !</h6>
+                    <form class="mt-4">
+                        <input type="email" class="form-control" placeholder="enter email">
+                        <a href="#" class="btn btn-primary mt-3">Submit <i data-feather="send"
+                                class="icon-xxs icon"></i></a>
+                    </form>
+                </div>
+                <div class="col-lg-5 offset-lg-1">
+                    <img class="img-fluid mt-sm-0 mt-5" src="images/subscribe-img.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section> -->
+    <!-- end subscription -->
+
+    
+
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Get In Touch</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" name="myForm" onsubmit="return validateForm()">
+                        <span id="error-msg"></span>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label class="fw-medium form-label" for="name">Name</label>
+                                        <input type="text" class="form-control" placeholder="Your name" id="name" />
+                                    </div>
+                                    <!-- end col -->
+                                    <div class="col-md-6 mb-3">
+                                        <label class="fw-medium form-label" for="email">Email</label>
+                                        <input type="email" class="form-control" placeholder="Your email" id="email" />
+                                    </div>
+                                    <!-- end col -->
+                                    <div class="col-md-6 mb-3">
+                                        <label class="fw-medium form-label" for="subject">Subject</label>
+                                        <input type="text" class="form-control" placeholder="your subject"
+                                            id="subject" />
+                                    </div>
+                                    <!-- end col -->
+                                    <div class="col-md-6 mb-3">
+                                        <label class="fw-medium form-label" for="number">Contact</label>
+                                        <input type="text" class="form-control" placeholder="+00 1234 5678 90"
+                                            id="number" />
+                                    </div>
+                                    <!-- end col -->
+                                    <div class="col-12 mb-3">
+                                        <label class="fw-medium form-label" for="comments">Message</label>
+                                        <textarea class="form-control" id="comments" placeholder="Enter your message..."
+                                            rows="5"></textarea>
+                                    </div>
+                                    <!-- end col -->
+                                    <div class="col-12">
+                                        <input type="submit" id="submit" name="send" class="btn btn-primary mt-2"
+                                            value="Send Information" />
+                                    </div>
+                                    <!-- end col -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end row -->
+                    </form>
+                    <!-- end form -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end modal -->
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+    <!-- feather icon -->
+    <script src="js/feather.js"></script>
+
+    <!-- client-slider -->
+    <script src="js/tiny-slider.js"></script>
+    <script src="js/tiny.init.js"></script>
+
+    <!-- moving letter -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    <script src="js/text-animation.init.js"></script>
+
+    <script src="js/app.js"></script>
+
+   <!-- button click -->
+   <script>
+$(document).ready(function(){
+  $("#calculate_uses").click(function(){
+    $('#ouses').show();
+                $('#opower').hide();
+  });
+  $("#calculate_power").click(function(){
+    $('#ouses').hide();
+                $('#opower').show(); 
+  });
+});
+</script>
+ 
+</body>
+
+</html>
---
+++ output_uses.php
@@ -0,0 +1,82 @@
+  <!-- start services -->
+  <section class="section hide" id="ouses">
+      <div class="container">
+          <div class="row justify-content-center text-center">
+              <div class="col-12 mb-4">
+                  <h4 class="fw-semibold mb-3">USES SIMULATION RESULT</h4>
+                  <!-- <h5 class="text-muted fw-normal">Don't you get what you want?</h5> -->
+              </div>
+          </div>
+          <div class="row">
+              <h6 class="my-4">Parameter Summary</h6>
+              <div class="col-4">
+                  <p>Tariff:</p>
+                  <p>Avg Consumption Monthly:</p>
+                  <p>Solar Capacity:</p>
+                  <p>Solar panel Cost:</p>
+              </div>
+              <div class="col-4">
+                  <p>Max Demand:</p>
+                  <p>Battery Size:</p>
+                  <p>Battery Price:</p>
+              </div>
+
+              <div class="col-4">
+                  <p>Year Leased:</p>
+                  <p>Unit Installed:</p>
+              </div>
+
+          </div>
+          <div class="row">
+              <h6 class="my-4">Overall Summary</h6>
+              <div class="col-md-4 mt-4 pt-2">
+                  <div class="service rounded px-4 py-md-5 py-3 active">
+                      <h6 class="my-4">Total Current Bill</h6>
+                      <p class="text-muted mb-4">
+                          Some quick example text
+                      </p>
+
+                  </div>
+              </div>
+              <div class="col-md-4 mt-4 pt-2">
+                  <div class="service rounded px-4 py-md-5 py-3 active">
+                      <h6 class="my-4">Total New Bill</h6>
+                      <p class="text-muted mb-4">
+                          Some quick example text
+                      </p>
+
+                  </div>
+              </div>
+              <div class="col-md-4 mt-4 pt-2">
+                  <div class="service rounded px-4 py-md-5 py-3 active">
+                      <h6 class="my-4">Total Investment Cost</h6>
+                      <p class="text-muted mb-4">
+                          Some quick example text
+                      </p>
+
+                  </div>
+              </div>
+              <div class="col-md-4 mt-4 pt-2">
+                  <div class="service rounded px-4 py-md-5 py-3 active">
+                      <h6 class="my-4">Total Saving</h6>
+                      <p class="text-muted mb-4">
+                          Some quick example text
+                      </p>
+                  </div>
+              </div>
+              <div class="col-md-4 mt-4 pt-2">
+                  <div class="service rounded px-4 py-md-5 py-3 active">
+                      <h6 class="my-4">ROI</h6>
+                      <p class="text-muted mb-4">
+                          Some quick example text
+                      </p>
+                  </div>
+              </div>
+              <div class="mt-5">
+                  <a href="#duses" class="btn btn-danger lh-base mb-4">Edit Parameter</a>
+              </div>
+
+          </div>
+      </div>
+  </section>
+  <!-- end servies -->
